f73f67e885f355ebdf6f850dbad5a49c456d18c5
Philip Koch <koch@prime-research.com>
2020-12-08 12:14:33 +0100

    day 1 minor cleanup

--- 2020/rust/day_1_report_repair/src/main.rs
+++ 2020/rust/day_1_report_repair/src/main.rs
@@ -8,11 +8,11 @@ fn get_file_contents_as_vec(file_name: &str) -> Vec<u32> {
 }
 
 // Part 1
-fn get_2_2020_summerands(numbers: &Vec<u32>) -> () {
+fn get_2_2020_summerands(numbers: &Vec<u32>, ex_result: &u32) -> () {
     for i in 0..numbers.len() {
         for j in i+1..numbers.len() {
             let add_result = numbers[i]+ numbers[j];
-            if add_result == 2020 {
+            if add_result == *ex_result {
                 println!("Found {} and {} = {}", numbers[i], numbers[j], add_result);
                 println!("Result is:  {}", numbers[i] * numbers[j]);
             }
@@ -21,12 +21,13 @@ fn get_2_2020_summerands(numbers: &Vec<u32>) -> () {
 }
 
 // Part 2
-fn get_3_2020_summerands(numbers: &Vec<u32>) -> () {
-    for i in 0..numbers.len() {
-        for j in i+1..numbers.len() {
-            for k in j+1..numbers.len() {
+fn get_3_2020_summerands(numbers: &Vec<u32>, ex_result: &u32) -> () {
+    let length = numbers.len();
+    for i in 0..length {
+        for j in i+1..length {
+            for k in j+1..length {
                 let add_result = numbers[i]+ numbers[j] + numbers[k];
-                if add_result == 2020 {
+                if add_result == *ex_result {
                     println!("Found {}, {} and {} = {}", numbers[i], numbers[j], numbers[k], add_result);
                     println!("Result is:  {}", numbers[i] * numbers[j] * numbers[k]);
                 }
@@ -35,12 +36,12 @@ fn get_3_2020_summerands(numbers: &Vec<u32>) -> () {
     }
 }
 
-
-
-
 fn main() {
     let numbers = get_file_contents_as_vec("src/input.txt");
-    get_2_2020_summerands(&numbers);
-    get_3_2020_summerands(&numbers);
-
+    let expected_result_sum: u32 = 2020;
+    println!("Searching 2 summerands in {} numbers that add up to {}", numbers.len(), expected_result_sum);
+    get_2_2020_summerands(&numbers, &expected_result_sum);
+    println!();
+    println!("Searching 3 summerands in {} numbers that add up to {}", numbers.len(), expected_result_sum);
+    get_3_2020_summerands(&numbers, &expected_result_sum);
 }
