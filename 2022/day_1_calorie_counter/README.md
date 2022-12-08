5fcb14a51ecf8412c002b6cc917e4825092cc7ef
Philip Koch <philipkoch@live.de>
2022-12-08 23:45:14 +0100

    added implementation for 2022-1-part2

--- 2022/day_1_calorie_counter/README.md
+++ 2022/day_1_calorie_counter/README.md
@@ -55,4 +55,4 @@ In the example above, the top three Elves are the fourth Elf (with 24000 Calorie
 
 Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 
-
+Answer: 
--- 2022/day_1_calorie_counter/src/main.rs
+++ 2022/day_1_calorie_counter/src/main.rs
@@ -7,20 +7,52 @@ fn get_file_contents_as_vec(file_name: &str) -> Vec<u32> {
         .collect()
 }
 
-fn main() {
-    let input = get_file_contents_as_vec("input");
+fn count_calories(puzzle_input: &Vec<u32>) -> Vec<u32> {
+    /* adds up the calories for each elf and returns an
+    unordered list of all calory-sums */
     let mut elves = vec![];
     let mut calorie_counter:u32 = 0;
-    for line in input.iter() {
+    for line in puzzle_input.iter() {
         if line == &0 {
-            if elves.len() > 0 && calorie_counter > elves[0] {
-                elves.insert(0, calorie_counter);
-            }
+            // empty line means new elf
             elves.push(calorie_counter);
             calorie_counter = 0;
         } else {
             calorie_counter += line;
         }
     }
-    println!("Most calories: {}", elves[0])
+    return elves;
+}
+fn part_one(input: &Vec<u32>) -> u32 {
+    let mut elves = count_calories(input);
+    elves.sort();
+    elves.reverse();
+    return elves[0];
+}
+
+fn part_two(input: &Vec<u32>) -> u32 {
+    let mut elves = count_calories(input);
+    elves.sort();
+    elves.reverse();
+    elves.resize(3, 0);
+    let mut calories = 0;
+    for elf in elves.iter() {
+        calories += elf;
+    }
+    return calories;
+}
+
+fn main() {
+    let input = get_file_contents_as_vec("input");
+    println!("Part 1:");
+    println!("Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?");
+    let part_one_answer = part_one(&input);
+    println!("Answer: {}", part_one_answer);
+    assert!(67622 == part_one_answer, "{} is the wrong answer!", part_one_answer);
+    println!();
+    println!("Part 2:");
+    println!("Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?");
+    let part_two_answer = part_two(&input);
+    println!("Answer: {}", part_two_answer);
+    assert!(201491 == part_two_answer, "{} is the wrong answer!", part_two_answer);
 }
